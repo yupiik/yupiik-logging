8fb65feb48be6a8f008794df094327f0bff70b0a
Romain Manni-Bucau <rmannibucau@gmail.com>
2022-03-16 10:14:37 +0100

    make JsonFormatter depend on JSON-P only and no more JSON-B

--- yupiik-logging-jul/src/main/java/io/yupiik/logging/jul/formatter/JsonFormatter.java
+++ yupiik-logging-jul/src/main/java/io/yupiik/logging/jul/formatter/JsonFormatter.java
@@ -15,22 +15,22 @@
  */
 package io.yupiik.logging.jul.formatter;
 
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import javax.json.bind.JsonbConfig;
-import javax.json.bind.annotation.JsonbProperty;
-import javax.json.bind.annotation.JsonbPropertyOrder;
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class JsonFormatter extends Formatter {
-    private final Jsonb mapper = JsonbBuilder.create(new JsonbConfig().setProperty("johnzon.skip-cdi", true));
+    private static final ZoneId UTC = ZoneId.of("UTC");
+
+    private final JsonBuilderFactory builder = Json.createBuilderFactory(Map.of());
 
     private boolean useUUID;
     private boolean formatMessage = true;
@@ -45,84 +45,38 @@ public class JsonFormatter extends Formatter {
 
     @Override
     public String format(final LogRecord record) {
-        final var object = new Record(record, formatMessage ? formatMessage(record) : record.getMessage());
+        final var json = builder.createObjectBuilder();
         if (useUUID) {
-            object.uuid = UUID.randomUUID().toString();
+            json.add("uuid", UUID.randomUUID().toString());
         }
-        return mapper.toJson(object) + '\n';
+        json.add("timestamp", OffsetDateTime.ofInstant(Instant.ofEpochMilli(record.getMillis()), UTC).toString());
+        if (record.getLevel() != null) {
+            json.add("level", record.getLevel().getName());
+        }
+        if (record.getLoggerName() != null) {
+            json.add("logger", record.getLoggerName());
+        }
+        if (record.getSourceMethodName() != null) {
+            json.add("method", record.getSourceMethodName());
+        }
+        final var message = formatMessage ? formatMessage(record) : record.getMessage();
+        if (message != null) {
+            json.add("message", message);
+        }
+        if (record.getThrown() != null) {
+            json.add("exception", toString(record.getThrown()));
+        }
+        if (record.getSourceClassName() != null) {
+            json.add("class", record.getSourceClassName());
+        }
+        return json.build().toString() + '\n';
     }
 
-    @JsonbPropertyOrder({
-            "uuid",
-            "timestamp",
-            "level",
-            "logger",
-            "clazz",
-            "method",
-            "message",
-            "exception"
-    })
-    public static class Record {
-        private static final ZoneId UTC = ZoneId.of("UTC");
-
-        @JsonbProperty("class")
-        private final String clazz;
-        private final String method;
-        private final String message;
-        private final String logger;
-        private final String exception;
-        private final String level;
-        private final OffsetDateTime timestamp;
-        private String uuid;
-
-        private Record(final LogRecord record, final String message) {
-            this.timestamp = OffsetDateTime.ofInstant(Instant.ofEpochMilli(record.getMillis()), UTC);
-            this.level = record.getLevel().getName();
-            this.logger = record.getLoggerName();
-            this.clazz = record.getSourceClassName();
-            this.method = record.getSourceMethodName();
-            this.message = message;
-            this.exception = record.getThrown() == null ? null : toString(record.getThrown());
+    private String toString(final Throwable thrown) {
+        final StringWriter w = new StringWriter();
+        try (final PrintWriter writer = new PrintWriter(w)) {
+            thrown.printStackTrace(writer);
         }
-
-        public String getUuid() {
-            return uuid;
-        }
-
-        public String getClazz() {
-            return clazz;
-        }
-
-        public String getMethod() {
-            return method;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public String getLogger() {
-            return logger;
-        }
-
-        public String getException() {
-            return exception;
-        }
-
-        public String getLevel() {
-            return level;
-        }
-
-        public OffsetDateTime getTimestamp() {
-            return timestamp;
-        }
-
-        private String toString(final Throwable thrown) {
-            final StringWriter w = new StringWriter();
-            try (final PrintWriter writer = new PrintWriter(w)) {
-                thrown.printStackTrace(writer);
-            }
-            return w.toString();
-        }
+        return w.toString();
     }
 }
